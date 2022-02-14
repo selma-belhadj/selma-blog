6b3b9bf6dd53c508e300766edb0bf96033555f17
salam <es_belhadj@esi.dz>
2022-02-14 21:56:14 +0100

    style the homepage

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Selma's Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create"  >New Blog</a>
+
+
+            </div>
+        </nav>
+      );
+}
+ 
+export default Navbar;
